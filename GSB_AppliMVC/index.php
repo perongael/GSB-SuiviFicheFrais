4e67a16d27025518c5b0cea438e42e60b4c42b6a
Peron Gaël <43822054+perongael@users.noreply.github.com>
2019-03-11 18:42:48 +0100

    Add files via upload

--- GSB_AppliMVC/index.php
+++ GSB_AppliMVC/index.php
@@ -5,14 +5,12 @@
  * PHP Version 7
  *
  * @category  PPE
- * @package   GSB
- * @author    Réseau CERTA <contact@reseaucerta.org>
- * @author    José GIL <jgil@ac-nice.fr>
- * @copyright 2017 Réseau CERTA
- * @license   Réseau CERTA
- * @version   GIT: <0>
+ * @package   GSB 
+ * @author    Peron Gaël
+ * @copyright 2018 - 2019 
  * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
  */
+ namespace gsb;
 require_once 'includes/fct.inc.php';
 require_once 'includes/class.pdogsb.inc.php';
 session_start();
@@ -34,30 +32,30 @@ elseif (empty($uc))
 	$_SESSION['chemin'] = 'accueil';
 	}
 switch ($uc) {
-case 'connexion':
-	include 'controleurs/c_connexion.php';
-	break;
-case 'accueil':
-	include 'controleurs/c_accueil.php';
-	break;
-case 'gererFrais':
-	include 'controleurs/c_gererFrais.php';
-	break;
-case 'etatFrais':
-	include 'controleurs/c_etatFrais.php';
-	break;
-case 'validerFrais':
-	include 'controleurs/c_validerFrais.php';
-	break;
-case 'voirFicheFrais':
-	include 'controleurs/c_voirFicheFrais.php';
-	break;
-case 'imprimerFicheFrais':
-	include 'controleurs/c_imprimer.php';
-	break;
-case 'deconnexion':
-	include 'controleurs/c_deconnexion.php';
-	break;
+    case 'connexion':
+            include 'controleurs/c_connexion.php';
+            break;
+    case 'accueil':
+            include 'controleurs/c_accueil.php';
+            break;
+    case 'gererFrais':
+            include 'controleurs/c_gererFrais.php';
+            break;
+    case 'etatFrais':
+            include 'controleurs/c_etatFrais.php';
+            break;
+    case 'validerFrais':
+            include 'controleurs/c_validerFrais.php';
+            break;
+    case 'voirFicheFrais':
+            include 'controleurs/c_voirFicheFrais.php';
+            break;
+    case 'imprimerFicheFrais':
+            include 'controleurs/c_imprimer.php';
+            break;
+    case 'deconnexion':
+            include 'controleurs/c_deconnexion.php';
+            break;
 }
 if ($uc != 'imprimerFicheFrais')
 {
